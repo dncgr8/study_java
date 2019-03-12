fd712e58df4ff4b46ae71c1e0f05c3bd698fb82f
jaelin <dncgr8@gmail.com>
2019-03-12 23:57:02 +0900

    extends abstract, implements interface

---
+++ sukiri/Fool.java
@@ -0,0 +1,26 @@
+public class Fool extends Character implements Human {
+    @Override
+    public void attack(Matango m) {
+        System.out.println ();
+    }
+
+    @Override
+    public void talk() {
+
+    }
+
+    @Override
+    public void watch() {
+
+    }
+
+    @Override
+    public void hear() {
+
+    }
+
+    @Override
+    public void run() {
+        super.run ();
+    }
+}
