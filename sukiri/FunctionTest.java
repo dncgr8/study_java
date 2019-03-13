bef28c54c86f9b66405bbaaee7b17d1fb40da7ac
jaelin <dncgr8@gmail.com>
2019-03-13 23:34:52 +0900

    ramda_00

---
+++ sukiri/FunctionTest.java
@@ -0,0 +1,14 @@
+import java.util.function.IntBinaryOperator;
+
+public class FunctionTest {
+    public static int sub (int a, int b){
+        return a-b;
+    }
+
+    public static void main(String[] args) {
+       IntBinaryOperator func =  FunctionTest::sub;
+
+       int a= func.applyAsInt (5,3);
+        System.out.println ("5-3="+a);
+    }
+}
