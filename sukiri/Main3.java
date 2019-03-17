3cb974c7e7374365f560807126c54762eb826792
jaelin <dncgr8@gmail.com>
2019-03-17 17:46:27 +0900

    Monster, Main3 added

---
+++ sukiri/Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args) {
+        Wizard w= new Wizard ();
+        Character c= w;
+        Matango m= new Matango ();
+        c.name="JAELIN";
+        c.attack (m);
+       //c.fireball ();
+    }
+}
